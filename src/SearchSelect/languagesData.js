fb13029a25e66f032e4b030aa3df157e6279c5ae
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2019-11-27 19:54:10 -0300

    ✨Add more programming languages options

--- src/SearchSelect/languagesData.js
+++ src/SearchSelect/languagesData.js
@@ -1,11 +1,29 @@
+// Top Active languages found at https://githut.info/
+
 export const availableLanguages = [
     {
         value: '',
         label: 'None'
     },
     {
+      value: 'Arduino',
+      label: 'Arduino'
+    },
+    {
+      value: 'ASP',
+      label: 'ASP'
+    },
+    {
       value: 'C',
       label: 'C'
+    },
+    {
+      value: 'Clojure',
+      label: 'Clojure'
+    },
+    {
+      value: 'CSS',
+      label: 'CSS'
     },
     {
       value: 'C++',
@@ -16,6 +34,38 @@ export const availableLanguages = [
       label: 'C#'
     },
     {
+      value: 'CoffeeScript',
+      label: 'CoffeeScript'
+    },
+    {
+      value: 'Emacs',
+      label: 'Emacs Lisp'
+    },
+    {
+      value: 'Elixir',
+      label: 'Elixir'
+    },
+    {
+      value: 'Dart',
+      label: 'Dart'
+    },
+    {
+      value: 'Go',
+      label: 'Go'
+    },
+    {
+      value: 'Groovy',
+      label: 'Groovy'
+    },
+    {
+      value: 'Haskell',
+      label: 'Haskell'
+    },
+    {
+      value: 'Lua',
+      label: 'Lua'
+    },
+    {
       value: 'Java',
       label: 'Java'
     },
@@ -24,11 +74,63 @@ export const availableLanguages = [
       label: 'JavaScript'
     },
     {
+      value: 'Matlab',
+      label: 'Matlab'
+    },
+    {
+      value: 'Objective-C',
+      label: 'Objective-C'
+    },
+    {
+      value: 'PHP',
+      label: 'PHP'
+    },
+    {
+      value: 'Perl',
+      label: 'Perl'
+    },
+    {
+      value: 'PowerShell',
+      label: 'PowerShell'
+    },
+    {
+      value: 'Puppet',
+      label: 'Puppet'
+    },
+    {
       value: 'Python',
       label: 'Python'
     },
     {
+      value: 'R',
+      label: 'R'
+    },
+    {
       value: 'Ruby',
       label: 'Ruby'
+    },
+    {
+      value: 'Rust',
+      label: 'Rust'
+    },
+    {
+      value: 'Scala',
+      label: 'Scala'
+    },
+    {
+      value: 'Shell',
+      label: 'Shell'
+    },
+    {
+      value: 'Swift',
+      label: 'Swift'
+    },
+    {
+      value: 'TeX',
+      label: 'TeX'
+    },
+    {
+      value: 'VimL',
+      label: 'VimL'
     }
   ];
