8aa3eb552746c156d69d72311a5ac684e4303b4a
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2019-11-27 19:39:23 -0300

    Updates

---
+++ precache-manifest.118010677360ccd4fd1791dedef98b14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e09d1227353f1c2ec9e9b2880a465c8",
+    "url": "/github-repo-language-search/index.html"
+  },
+  {
+    "revision": "3d86b04d572a0d7c250f",
+    "url": "/github-repo-language-search/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "546d64adde6811d808fd",
+    "url": "/github-repo-language-search/static/js/2.5e42b4d3.chunk.js"
+  },
+  {
+    "revision": "3d86b04d572a0d7c250f",
+    "url": "/github-repo-language-search/static/js/main.f9f78b0e.chunk.js"
+  },
+  {
+    "revision": "18243dcce089b2e213ef",
+    "url": "/github-repo-language-search/static/js/runtime-main.10aec781.js"
+  }
+]);
